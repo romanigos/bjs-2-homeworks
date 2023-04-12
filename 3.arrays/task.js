8247aa02c04d4edd0a3cfedb4d13dc3936c9d47d
Roman Igoshev <Roman.parkflyer@mail.ru>
2023-04-12 19:44:34 +0500

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length === arr2.length && arr1.every((item) => arr1[item] === arr2[item]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	return users.filter(user => gender === user.gender)
+		.map(user => user.age)
+		.reduce((summator, age, index, userAges) => {
+			if (index === userAges.length - 1) {
+				return (summator + age) / userAges.length;
+			}
+			return summator + age;
+		}, 0);
 }
