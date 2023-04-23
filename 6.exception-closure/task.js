c8f9b2babf00877459ff8e509f51f17e2c8c89b5
Roman Igoshev <Roman.parkflyer@mail.ru>
2023-04-23 08:33:47 +0500

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,52 @@
-﻿
+﻿function parseCount(number) {
+	if (isNaN(number)) {
+		throw new Error('Невалидное значение');
+	}
+	return Number.parseFloat(number);
+}
+
+
+function validateCount(number) {
+	try {
+		return parseCount(number);
+	} catch (error) {
+		return error;
+	}
+}
+
+
+class Triangle {
+	constructor(a, b, c) {
+        this.a = a;
+		this.b = b;
+		this.c = c;
+        
+		if (a + b < c || b + c < a || a + c < b) {
+			throw new Error('Треугольник с такими сторонами не существует');
+		};
+			}
+
+	get perimeter() {
+		return Number.parseFloat((this.a + this.b + this.c));
+	}
+
+	get area() {
+		let p = 0.5 * this.perimeter;
+		return Number.parseFloat(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (error) {
+		return {
+			get area() {
+				return 'Ошибка! Треугольник не существует';
+			},
+			get perimeter() {
+				return 'Ошибка! Треугольник не существует';
+			}
+		}
+	}
+}
